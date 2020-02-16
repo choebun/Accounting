3242b2fd8aac3b48baada85cbbda50b533edcd7f
Joey Chen <jay.hatelove@gmail.com>
2020-02-16 15:38:56 +0800

    [feat] add BudgetController and Create() action, add a prototype view

---
+++ AccountingWeb/Controllers/BudgetsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AccountingWeb.Controllers
+{
+    public class BudgetsController : Controller
+    {
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
