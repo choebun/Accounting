e988c9ab0b6814d215140ed798fa577ca60a09f6
Joey Chen <jay.hatelove@gmail.com>
2020-02-16 17:39:23 +0800

    [feat] use Autofac as DI solution and Register BudgetService as IBudgetService

---
+++ AccountingWeb/Models/Services/BudgetService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AccountingWeb.Models.Services
+{
+    public class BudgetService : IBudgetService
+    {
+        public void Save(string yearMonth, int amount)
+        {
+        }
+    }
+}
