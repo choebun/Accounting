9a223c2a65642e2e779fc2c5f3a6e8725e784dee
User <o88ma@hotmail.com>
2020-02-20 17:40:14 +0800

    cauculate amount

--- AccountingWeb/Controllers/BudgetsController.cs
+++ AccountingWeb/Controllers/BudgetsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AccountingWeb.Models.Entities;
 using AccountingWeb.Models.Services;
 
 namespace AccountingWeb.Controllers
@@ -34,6 +35,36 @@ namespace AccountingWeb.Controllers
 
         public ActionResult Query(string YearMonth_Start, string YearMonth_End)
         {
+            Dictionary<DateTime, decimal> MyDic = new Dictionary<DateTime, decimal>();
+
+            using (var dbContext = new AccountingEntities())
+            {
+
+                var BundleList = dbContext.Budgets.ToList();
+                foreach (Budget bufgetList in BundleList)
+                {
+                    DateTime firstDay = DateTime.ParseExact(bufgetList.YearMonth + "01", "yyyyMMdd", null);
+                    var daysinMonth = DateTime.DaysInMonth(firstDay.Year, firstDay.Month);
+                    var dayAmnout = bufgetList.Amount / daysinMonth;
+
+                    MyDic.Add(firstDay, dayAmnout);
+                }
+            }
+
+            //DateTime dt1 = DateTime.ParseExact(QueryFrom, "yyyyMMdd", null);
+            //DateTime dt2 = DateTime.ParseExact(QueryTo, "yyyyMMdd", null);
+            //var ts = (dt2 - dt1).Days+1;
+
+            DateTime queryFromMonthDay = DateTime.ParseExact(YearMonth_Start, "yyyyMMdd", null);
+            var queryFormdaysinMonth = DateTime.DaysInMonth(queryFromMonthDay.Year, queryFromMonthDay.Month);
+            var monthFromDays = queryFormdaysinMonth - queryFromMonthDay.Day + 1;
+            var startMonthAccount = monthFromDays * MyDic[DateTime.ParseExact(YearMonth_Start, "yyyyMMdd", null)];
+
+            DateTime queryToMonthDay = DateTime.ParseExact(YearMonth_End, "yyyyMMdd", null);
+            var queryTodaysinMonth = DateTime.DaysInMonth(queryToMonthDay.Year, queryToMonthDay.Month);
+            var monthToDays = queryTodaysinMonth - queryToMonthDay.Day + 1;
+            var EndMonthAccount = monthToDays * MyDic[DateTime.ParseExact(YearMonth_End, "yyyyMMdd", null)];
+
             ViewBag.Message = YearMonth_Start;
             return View();
         }
